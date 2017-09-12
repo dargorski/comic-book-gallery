7adef9cc5662f4007666c13edc242fc671ecbfc6
dariuszgoorski <dariuszgoorski@gmail.com>
2017-09-12 19:46:30 +0200

    Added Comic Book repository

--- src/ComicBookGallery/Controllers/ComicBooksController.cs
+++ src/ComicBookGallery/Controllers/ComicBooksController.cs
@@ -9,6 +9,7 @@ namespace ComicBookGallery.Controllers
 {
     public class ComicBooksController : Controller
     {
+
         public ActionResult Detail()
         {
             var comicBook = new ComicBook()
@@ -28,23 +29,7 @@ namespace ComicBookGallery.Controllers
             };
 
             return View(comicBook);
-
-           
-
-            ViewBag.SeriesTitle = "The Amazing Spider-Man";
-            ViewBag.IssueNumber = 700;
-            ViewBag.Description = "<p>Final issue! Witness the final hours of Doctor Octopus' life and his one, last, great act of revenge! Even if Spider-Man survives... <strong>will Peter Parker?</strong></p>";
-            ViewBag.Artists = new string[]
-            {
-                "Script: Dan Slott",
-                "Pencils: Humberto Ramos",
-                "Inks: Victor Olazaba",
-                "Colors: Edgar Delgado",
-                "Letters: Chris Eliopoulos"
-            };
-
-            return View();      
-             
+            
         }
     }
 }
