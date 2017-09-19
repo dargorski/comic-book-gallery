87b4431a5ccbacf11acf70db81283b437ffacec5
Dariusz Gorski <dariusz.gorski@comarch.pl>
2017-09-19 15:46:57 +0200

    Added Series Repository

---
+++ src/ComicBookGallery/Data/SeriesRepository.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using ComicBookGallery.Models;
+
+namespace ComicBookGallery.Data
+{
+    public class SeriesRepository
+    {
+        public Series[] GetSeries()
+        {
+            return Data.Series;
+        }
+
+        public Series GetSeriesDetail(int ID)
+        {
+            Series seriesToReturn = null;
+            
+
+            foreach (var series in Data.Series)
+            {
+                if (series.ID == ID)
+                {
+                    seriesToReturn = series;
+                    break;
+                }
+            }
+
+            if(seriesToReturn != null)
+            {
+                var comicBooks = new ComicBook[0];
+
+                foreach (var comicBook in Data.ComicBooks)
+                {
+                    if (comicBook.Series != null && comicBook.Series.ID == ID)
+                    {
+                        Array.Resize(ref comicBooks, comicBooks.Length + 1);
+                        comicBooks[comicBooks.Length - 1] = comicBook;
+                    }
+                }
+                seriesToReturn.Issues = comicBooks;
+
+                
+                 
+
+            }
+            return seriesToReturn;
+        }
+
+    
+    }
+}
